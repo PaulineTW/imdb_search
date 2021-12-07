053dc582757b3f3e052f330a48c5374251c80909
pauline rouger <paulinerouge@hotmail.com>
2021-12-07 12:24:22 +0800

    Updated Movies models + controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,7 @@
 class MoviesController < ApplicationController
   def index
     if params[:query].present?
-      sql_query = " \
-        movies.title @@ :query \
-        OR movies.synopsis @@ :query \
-        OR directors.first_name @@ :query \
-        OR directors.last_name @@ :query \
-      "
-      @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+      @movies = Movie.search_by_title_and_synopsis(params[:query])
     else
       @movies = Movie.all
     end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,12 @@
 class Movie < ApplicationRecord
   belongs_to :director
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :title, :synopsis ],
+    associated_against: {
+      director: [ :first_name, :last_name ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
