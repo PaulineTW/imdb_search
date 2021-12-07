227b7fd188b7c8f634bbb29f9f371a0455cd8822
pauline rouger <paulinerouge@hotmail.com>
2021-12-07 11:57:15 +0800

    Searching multiple terms in movies index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,13 @@
 class MoviesController < ApplicationController
   def index
     if params[:query].present?
-      @movies = Movie.where("title ILIKE ?", "%#{params[:query]}%")
+      sql_query = " \
+        movies.title @@ :query \
+        OR movies.synopsis @@ :query \
+        OR directors.first_name @@ :query \
+        OR directors.last_name @@ :query \
+      "
+      @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
     else
       @movies = Movie.all
     end
