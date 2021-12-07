65267e6c03715f7ba2fa409c4fe68f584363eb22
pauline rouger <paulinerouge@hotmail.com>
2021-12-07 11:54:20 +0800

    Movies index change for search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,9 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    if params[:query].present?
+      @movies = Movie.where("title ILIKE ?", "%#{params[:query]}%")
+    else
+      @movies = Movie.all
+    end
   end
 end
